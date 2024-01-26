a966a179246825892dac7bae1d6d20d1af0cc446
yordanos-bogale5 <bogaleyordanos64@gmail.com>
2024-01-27 00:40:38 +0300

    slider

---
+++ lib/slider.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Carousel Slider'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CarouselSlider(
+                items: [
+                  _buildCarouselItem('Slide 1', Colors.blue),
+                  _buildCarouselItem('Slide 2', Colors.green),
+                  _buildCarouselItem('Slide 3', Colors.orange),
+                  _buildCarouselItem('Slide 4', Colors.purple),
+                  // Add more items as needed
+                ],
+                options: CarouselOptions(
+                  height: 300.0,
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 3),
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  aspectRatio: 16 / 9,
+                  onPageChanged: (index, reason) {
+                    print('Page changed: $index');
+                  },
+                  scrollDirection: Axis.horizontal,
+                  pauseAutoPlayOnTouch: true,
+                  pauseAutoPlayOnManualNavigate: true,
+                  pauseAutoPlayInFiniteScroll: true,
+                ),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your button logic here
+                },
+                child: Text('Click Me'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCarouselItem(String text, Color color) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
